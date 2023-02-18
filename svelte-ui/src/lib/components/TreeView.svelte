636652f7e5a00cfd91b3a2001a86ce9beac828ef
bhushan217 <bhushan217@gmail.com>
2023-02-18 20:01:54 +0530

    tree view component

---
+++ svelte-ui/src/lib/components/TreeView.svelte
@@ -0,0 +1,81 @@
+<script context="module">
+	// retain module scoped expansion state for each tree node
+	const _expansionState = {
+		/* treeNodeId: expanded <boolean> */
+	}
+</script>
+<script>
+//	import { slide } from 'svelte/transition'
+	export let tree
+	const {label,description, isRoot, children} = tree
+
+	let expanded = _expansionState[label] || false
+	const toggleExpansion = () => {
+		expanded = _expansionState[label] = !expanded
+	}
+	$: arrowDown = expanded
+</script>
+
+	<ul class="tree-continer" class:isRoot>
+		{#if children}
+			<li on:click={toggleExpansion} on:keyup={toggleExpansion}
+       class="tree-node has-child" class:arrowDown>
+				<div class="tree-node-header">{label}</div>
+				<div class="tree-node-body">{description??'Amit'}</div>
+			</li>
+			{#if expanded}
+				{#each children as child}
+					<svelte:self tree={child} />
+				{/each}
+			{/if}
+		{:else}
+			<li class="tree-node no-child">
+				{label}
+			</li>
+		{/if}
+  </ul>
+
+<style lang="scss">
+	@use '../../styles' as *;
+  .tree-continer {
+    width: 100%;
+		user-select: none;
+		padding-left: .7rem;
+    &:not(.isRoot) {
+      border-left: 1px solid var(--input-bg);
+    }
+    .tree-node{
+      padding: .3rem .8rem;
+      position: relative;
+      &.has-child:before {
+        content: ' ';
+        @include absolute('lt', .2rem, .6rem);
+        margin-left: -.3rem;
+        font-size: .5rem;
+        cursor: pointer;
+        display: inline-block;
+        width: 1em;
+        height: 1em;
+        border-style: solid;
+        border-width: 2px 2px 0 0;
+        border-color: var(--primary, #888);
+        transform: rotate(45deg);
+        @include tranzision;
+      }
+      &.has-child.arrowDown:before {
+        transform: rotate(135deg);
+      }
+      .tree-node-header{
+        //background-color: var(--accent);
+        font-weight: 700;
+        display: inline-block;
+      }
+      .tree-node-body{
+        //background-color: var(--tertiary);
+        display: inline-block;
+        font-size: .7em;
+        color: var(--accent);
+      }
+    }
+  }
+</style>
--- svelte-ui/src/lib/pages/Page.svelte
+++ svelte-ui/src/lib/pages/Page.svelte
@@ -1,15 +1,69 @@
 <script>
 	import MaskedInput from '../fields/MaskedInput.svelte';
 	let phone = '';
+
+  import TreeView from '../components/TreeView.svelte'
+
+  const tree = {
+    isRoot: true,
+    label: "USA",
+    description: 'This  is country',    
+    children: [
+      {
+        label: "Florida",
+        description: 'This  is state', 
+        children: [
+          {
+            label: "Jacksonville"
+          },
+          {
+            label: "Orlando",
+            description: 'This  is state', 
+            children: [
+              {label: "Disney World"},
+              {label: "Universal Studio"},
+              {label: "Sea World"},
+            ]
+          },
+          {label: "Miami"},
+        ]
+      },
+      {
+        label: "California",
+        description: 'This  is state',
+        children: [
+          {label: "San Francisco"},
+          {label: "Los Angeles"},
+          {label: "Sacramento"},
+        ]
+      },
+    ],
+  }
 </script>
 
-<div class="form-control">  
+<div class="form-group">  
   <label class="field-label" for="masked-phone">
     Phone
   </label>
     <MaskedInput required={true} id="masked-phone" mask="+99-999-999-9999" bind:value={phone}
     />
+  <div class="description">
+    phone = {phone}
+  </div>
 </div>
-<div>
-	phone = {phone}
+
+
+<div class="form-group"> 
+  <TreeView {tree} />
 </div>
+
+<style lang="scss">
+  .form-group{
+    margin-top: 1rem;
+    border-bottom: solid 1px var(--shadow-color);
+    @extend .shadow !optional;
+    .description{
+      
+    }
+  }
+</style>
